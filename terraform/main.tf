87333d8f437ac8b64e5aac93ac3e4fb1631a0077
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-16 17:06:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,9 @@ resource "tls_private_key" "strapi_key" {
   rsa_bits  = 4096
 }
 
-# Create AWS key pair
+# Create AWS key pair with unique name
 resource "aws_key_pair" "strapi_key" {
-  key_name   = "strapi-ec2-key"
+  key_name   = "strapi-key-unique-01"
   public_key = tls_private_key.strapi_key.public_key_openssh
 }
 
@@ -21,9 +21,11 @@ resource "local_file" "private_key" {
   file_permission = "0400"
 }
 
-# Security group
+# Security group with unique name
 resource "aws_security_group" "strapi_sg" {
-  name = "strapi-ec2-sg"
+  name        = "strapi-sg-unique-01"
+  description = "Security group for Strapi EC2 instance"
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 22
@@ -45,33 +47,36 @@ resource "aws_security_group" "strapi_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "Strapi-SG-Unique-01"
+  }
 }
 
-# EC2 instance
+# EC2 instance with unique name
 resource "aws_instance" "strapi_ec2" {
   ami                    = var.ami_id
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.strapi_key.key_name
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
- user_data = <<-EOF
-#!/bin/bash
-apt update -y
-apt install -y docker.io
-systemctl start docker
-systemctl enable docker
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install -y docker.io
+    systemctl start docker
+    systemctl enable docker
 
-docker run -d \
--p 1337:1337 \
--e NODE_ENV=production \
--e APP_KEYS=key1,key2,key3,key4 \
---name strapi \
-${var.docker_image}
-EOF
-
+    docker run -d \
+      -p 1337:1337 \
+      -e NODE_ENV=production \
+      -e APP_KEYS=key1,key2,key3,key4 \
+      --name strapi-app-unique-01 \
+      ${var.docker_image}
+  EOF
 
   tags = {
-    Name = "Strapi-EC2"
+    Name = "Strapi-EC2-Unique-01"
   }
 }
 
