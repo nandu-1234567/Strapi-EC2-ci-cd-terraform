4839f938481b101690dfe71ba1d4773627e9999d
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-16 17:14:04 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,16 +7,10 @@ variable "aws_region" {
 variable "ami_id" {
   description = "AMI ID to use for the EC2 instance"
   type        = string
-  default     = "ami-0b6c6ebed2801a5cb"
+  default     = "ami-0b6c6ebed2801a5cb"  # Ubuntu 22.04 LTS in us-east-1
 }
 
 variable "docker_image" {
   description = "Docker image to run Strapi"
   type        = string
 }
-
-variable "vpc_id" {
-  description = "VPC ID where the security group will be created"
-  type        = string
-}
-
