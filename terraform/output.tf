646ba09faca16a85c03904d9023b3ef8aebf6e9b
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-16 13:20:44 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
-  value = aws_instance.strapi.public_ip
+  value = aws_instance.strapi_ec2.public_ip
+}
+
+output "private_key_path" {
+  value = local_file.private_key.filename
 }
