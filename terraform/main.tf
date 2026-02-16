d80962b6e0ffaefec276aa2851188dcdaeccaf2b
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-16 13:19:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,55 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "strapi" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+resource "tls_private_key" "strapi_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-  security_groups = ["strapi-sg"]
+resource "aws_key_pair" "strapi_key" {
+  key_name   = "strapi-key"
+  public_key = tls_private_key.strapi_key.public_key_openssh
+}
+
+# Save private key locally (optional)
+resource "local_file" "private_key" {
+  content  = tls_private_key.strapi_key.private_key_pem
+  filename = "strapi-key.pem"
+  file_permission = "0400"
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Allow SSH and Strapi"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Strapi Port"
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "strapi_ec2" {
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.strapi_key.key_name
+  vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -20,30 +63,5 @@ resource "aws_instance" "strapi" {
 
   tags = {
     Name = "Strapi-EC2"
-  }
-}
-
-resource "aws_security_group" "strapi_sg" {
-  name = "strapi-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
