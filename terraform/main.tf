35265e9df2ee29e04dbf479bacf18da5d5fb6f59
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-16 16:43:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,28 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "tls_private_key" "strapi-EC2-key" {
+# Generate SSH key
+resource "tls_private_key" "strapi_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "strapi-EC2-key" {
-  key_name   = "strapi-EC2-key"
+# Create AWS key pair
+resource "aws_key_pair" "strapi_key" {
+  key_name   = "strapi-ec2-key"
   public_key = tls_private_key.strapi_key.public_key_openssh
 }
 
+# Save private key locally
 resource "local_file" "private_key" {
   content         = tls_private_key.strapi_key.private_key_pem
   filename        = "strapi-key.pem"
   file_permission = "0400"
 }
 
-resource "aws_security_group" "strapi_Ec2-sg" {
-  name = "strapi_Ec2-sg"
+# Security group
+resource "aws_security_group" "strapi_sg" {
+  name = "strapi-ec2-sg"
 
   ingress {
     from_port   = 22
@@ -43,11 +47,12 @@ resource "aws_security_group" "strapi_Ec2-sg" {
   }
 }
 
+# EC2 instance
 resource "aws_instance" "strapi_ec2" {
   ami                    = var.ami_id
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.strapi_key.key_name
-  vpc_security_group_ids = [aws_security_group.strapi_Ec2-sg.id]
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
